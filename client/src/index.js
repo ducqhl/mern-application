507eb32fe242b4aa4c45ad3078f0bd8c83bb6eb9
ducqhl <duc.le@spiderbox.design>
2022-07-14 23:01:49 +0700

    remove dev tool error

--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,7 @@ const store = createStore(
   reducers,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "function" && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
